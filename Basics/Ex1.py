127f47b2c291f013773fd3a02d72c85d4071f190
[bhargav dharan] <bhargavdharan20@gmail.com>
2021-08-13 17:03:22 +0530

    Add file

---
+++ Basics/Ex1.py
@@ -0,0 +1,6 @@
+## Taking input from user
+
+name = input("Enter your name:")
+age = input("Enter your age:")
+print(name)
+print(age)
---
+++ Variables/Ex1.py
@@ -0,0 +1,9 @@
+## what is a variable?
+## (variable)x = 100(value)
+## ==> variables are entities of a program that holds a value.
+
+x = 100
+print(x)
+
+y = 9000
+print("y =",y)
