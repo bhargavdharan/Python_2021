6a71051d63ed1b0bc3be73e305d0a05b327050d4
[bhargav dharan] <bhargavdharan20@gmail.com>
2021-09-02 22:11:28 +0530

    Added files

---
+++ LetsUpgrade/Day-1/start.py
@@ -0,0 +1,29 @@
+a = "Hello, Python!"
+print(a)
+
+print('''
+(* *)
+ (/)
+(---)
+''')
+
+marks = 100
+name = "Ram"
+age = 21
+print("The marks of ",name," are ",marks," and age of ",name," is ",age)
+print("The marks of %s are %.2f and age is %d"%(name,marks,age))
+print(f"The marks of {name} is {marks} and age is {age}")
+
+'''
+---Built-in data types
+*List
+*Float
+*Boolean
+*string
+*Tuple
+*List
+*Dictionary
+*set
+'''
+a = ""
+dir(a)
