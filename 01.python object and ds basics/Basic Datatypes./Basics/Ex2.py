759e1af9045562ae2766a7b306d74b06dcd4cd4e
[bhargav dharan] <bhargavdharan20@gmail.com>
2021-09-07 12:06:19 +0530

    Added

---
+++ 01.python object and ds basics/Basic Datatypes./Basics/Ex2.py
@@ -0,0 +1,41 @@
+# variable assigning
+
+a = 10
+
+print(a)
+
+print(type(a))
+
+a = 12.35
+
+print(a)
+
+print(type(a))
+
+a = "Hello"
+
+print(a)
+
+print(type(a))
+
+a = [1,23,45,67,54]
+
+print("ordered sequence of objects==>",a)
+
+print(type(a))
+
+a = (1,2,3,4,5,6)
+
+print("ordered immutable sequence of objects==>",a)
+
+print(type(a))
+
+my_income = 1000
+
+tax_rate = 0.15
+
+my_taxes = my_income * tax_rate
+
+print(my_taxes)
+print(type(my_taxes))
+
